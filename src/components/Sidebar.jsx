fb8d38bf179ad89e20df774921ea8674ca8aed17
sibelly <sibellychan@hotmail.com>
2022-06-10 15:30:23 -0400

    App structure

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,30 @@
+import { PencilLine } from 'phosphor-react';
+import styles from './Sidebar.module.css';
+
+export function Sidebar (){
+    return (
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1605704311533-443f1f58874f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=50"
+                alt="Cover Picture"
+            />
+
+            <div className={styles.profile}>
+                <img
+                    className={styles.avatar}
+                    src="https://avatars.githubusercontent.com/u/7544858?v=4"
+                    alt="Profile Picture"/>
+                <strong>Sibelly Cavalcante</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    <PencilLine />
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    );
+}
